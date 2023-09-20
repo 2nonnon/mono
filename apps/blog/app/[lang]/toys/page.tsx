f90c3a69df734b4f8f4e49c1e125fb86e0f44118
2nonnon <lanpangzi0205@gmail.com>
2023-09-20 22:13:08 +0800

    feat: 调整

--- apps/blog/app/[lang]/toys/page.tsx
+++ apps/blog/app/[lang]/toys/page.tsx
@@ -13,38 +13,27 @@ export async function generateMetadata({ params: { lang } }: PageProps): Promise
 
 export default async function Page({ params: { lang } }: PageProps) {
   const copies = await getCopies(lang, 'toy')
-  const curPath = lang === 'en' ? '/toys' : `/${lang}/toys`
 
   const toys = [
     {
       name: copies.qrcode,
       icon: 'heroicons-outline:qrcode',
-      link: `${curPath}/qrcode`,
+      link: 'https://qrcode.non.fan',
     },
     {
       name: copies.mine,
       icon: 'game-icons:land-mine',
-      link: `${curPath}/minesweeper`,
+      link: 'https://minesweeper.non.fan',
     },
     {
       name: copies.color,
       icon: 'ic:outline-color-lens',
-      link: `${curPath}/color`,
+      link: 'https://color.non.fan',
     },
     {
       name: copies.image,
       icon: 'material-symbols:image-outline-rounded',
-      link: `${curPath}/image`,
-    },
-    {
-      name: copies.todo,
-      icon: 'ri:calendar-todo-line',
-      link: `${curPath}/todo`,
-    },
-    {
-      name: copies.audio,
-      icon: 'carbon:audio-console',
-      link: `${curPath}/audio`,
+      link: 'https://img.non.fan',
     },
   ]
 
@@ -53,7 +42,7 @@ export default async function Page({ params: { lang } }: PageProps) {
       <div className='max-w-screen-lg mx-auto w-full'>
         <div className='grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] place-items-stretch gap-6 my-10'>
           {toys.map(item => (
-            <Link key={item.link} className='surface-md bg-[var(--surface1)] rounded-lg text-lg font-medium flex flex-col items-center gap-2 py-4 px-6 h-full text-center' href={item.link}>
+            <Link key={item.link} className='surface-md bg-[var(--surface1)] rounded-lg text-lg font-medium flex flex-col items-center gap-2 py-4 px-6 h-full text-center' href={item.link} target='_blank'>
               <Icon className='text-[7rem]' icon={item.icon} />
               <p className='m-0'>{item.name}</p>
             </Link>
